fc53589a9c9259455719c7ca99592d0069a145cc
hayama.jun <hayama@raccoon.ne.jp>
2019-09-25 10:34:26 +0900

    サーバー機能に徐々転送機能を追加、デモHTML追加

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const fs = require('fs');
 const path = require('path');
 const url = require('url');
 
-const delayRegexp = /_([0-9]+)s/;
+const delayRegexp = /_([0-9]+)s(?=[._])/;
+const graduallyRegexp = /_g(?=[._])/;
 const defaultContentType = 'text/html';
 
 const basePath = path.dirname(process.argv[1]);
@@ -25,7 +26,10 @@ if (process.argv.length >= 4) {
     host = process.argv[3];
 }
 
-const handleRequest = (res, requestPath, delay, failOver) => {
+const requestIdMax = 100;
+let requestIdManager = 0;
+
+const handleRequest = (requestId, response, requestPath, delay, gradually, failOver) => {
     Promise.all(['', '/css', '/js'].map((subPath) => {
         return new Promise((resolve, reject) => {
             fs.access(basePath + subPath + requestPath, (err) => {
@@ -59,42 +63,80 @@ const handleRequest = (res, requestPath, delay, failOver) => {
             });
         });
     }).then((data) => {
-        setTimeout(() => {
-            let contentType = defaultContentType;
-            if (requestPath.endsWith(".css")) {
-                contentType = 'text/css';
+        let start = Date.now();
+        let sent = 0;
+        let sentLogged = 0;
+        let sentHeader = false;
+        let interval = Math.min(Math.floor(delay / 100), 50);
+        let sender = () => {
+            let now = Date.now();
+            let elapsed = now - start < 0 ? 0 : now - start;
+            let progress = elapsed >= delay ? data.length : Math.floor(data.length / delay * elapsed);
+            if (!sentHeader) {
+                let contentType = defaultContentType;
+                if (requestPath.endsWith(".css")) {
+                    contentType = 'text/css';
+                }
+                else if(requestPath.endsWith(".js")) {
+                    contentType = 'application/javascript';
+                }
+                response.writeHead(200, {
+                    'Content-Type': contentType,
+                    'Cache-Control': 'no-cache',
+                    'Transfer-Encoding': 'chunked'
+                });
+                sentHeader = true;
             }
-            else if(requestPath.endsWith(".js")) {
-                contentType = 'application/javascript';
+            if (progress > sent) {
+                response.write(data.substring(sent, progress), 'utf8', () => {
+                    if (progress >= (Math.floor(sentLogged / data.length * 10) + 1) / 10 * data.length) {
+                        let progressRatio = Math.floor(100 * progress / data.length);
+                        console.log(' '.repeat((new Date()).toString().length) + ' [' + '0'.repeat((''+requestIdMax).length - (''+requestId).length) +  requestId + '] ' + requestPath + ' sent: ' + (progressRatio >= 100 ? 'completed (' + elapsed + 'ms)' : progressRatio + '%'))
+                        sentLogged = progress;
+                    }
+                });
+                sent = progress;
             }
-            res.writeHead(200, {
-                'Content-Type': contentType,
-                'Cache-Control': 'no-cache'
-            });
-            res.write(data);
-            res.end();
-        }, delay);
+            if (data.length > sent) {
+                setTimeout(sender, interval);
+            }
+            else {
+                response.end();
+            }
+        };
+        setTimeout(sender, gradually ? interval : delay);
     }).catch((err) => {
         if (failOver) {
-            handleRequest(res, '/index.html', 0, false);
+            handleRequest(requestId, response, requestPath + 'index.html', 0, gradually, false);
         }
         else {
-            res.writeHead(404, {'Content-Type': defaultContentType});
-            res.write('Not Found');
-            res.end();
+            response.writeHead(404, {'Content-Type': defaultContentType});
+            response.write('Not Found', 'utf8', () => {
+                console.log(' '.repeat((new Date()).toString().length) + ' [' + '0'.repeat((''+requestIdMax).length - (''+requestId).length) +  requestId + '] ' + requestPath + ' 404 Not Found')
+            });
+            response.end();
         }
     });
 };
-server.on('request', (req, res) => {
-    let requestPath = url.parse(req.url).pathname;
+server.on('request', (request, response) => {
+    let requestId = (++requestIdManager) % requestIdMax;
+    let requestPath = url.parse(request.url).pathname;
     let delay = 0;
+    let gradually = false;
     let delayMatch = requestPath.match(delayRegexp);
     if (delayMatch) {
         requestPath = requestPath.replace(delayRegexp, '');
         delay = Number(delayMatch[1] * 1000);
     }
+    let graduallyMatch = requestPath.match(graduallyRegexp);
+    if (graduallyMatch) {
+        requestPath = requestPath.replace(graduallyMatch, '');
+        gradually = true;
+    }
 
-    console.log(new Date() + ' delay=' + delay + ' ' + requestPath);
-    handleRequest(res, requestPath, delay, requestPath === '/' || requestPath.endsWith('.html'));
+    console.log(new Date() + ' [' + '0'.repeat((''+requestIdMax).length - (''+requestId).length) +  requestId  + '] ' + requestPath + ' (delay=' + delay + 'ms' + (gradually ? ' gradually' : '') + ')');
+    handleRequest(requestId, response, requestPath, delay, gradually, requestPath.endsWith('/'));
 });
-server.listen(port, host);
+server.listen(port, host, () => {
+    console.log('Starting on http://' + (host ? host : 'localhost') + (port != 80 ? (':' + port) : ''));
+});
